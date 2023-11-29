7d30a75d1348fb8f1994e892098a46999033448e
hu.po <hugo.p.cmu@gmail.com>
2023-11-29 11:28:58 -0600

    looking clean

--- s.watch.sh
+++ s.watch.sh
@@ -1,1 +1,12 @@
-watch -n 0.1 "ps aux | grep python3"
+get_formatted_output() {
+    echo "@@@@@@@@@@@"
+    echo "🖥️ o python ps:"
+    echo "@@@@@@@@@@@"
+    ps aux | grep "python3" | grep -v grep
+    echo "@@@@@@@@@@@"
+}
+while true; do
+    clear
+    get_formatted_output
+    sleep 0.1
+done
