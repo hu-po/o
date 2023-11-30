62620d5a1452a4d8058c235586552d7a34b31cfc
hu.po <hugo.p.cmu@gmail.com>
2023-11-30 17:08:57 -0600

    no move on test

--- s.nex.test.sh
+++ s.nex.test.sh
@@ -3,6 +3,7 @@ set -e
 echo "🖥️ testing with gpt model_api and nex robot"
 export O_DEATH=20
 export O_MAX_STEPS=3
+export O_NEX_MOVE_ENABLED=0
 sh s.nuke.sh
 python3 o.body.py --model_api gpt --robot nex &
 python3 o.look.py --model_api gpt &
@@ -12,6 +13,7 @@ python3 o.talk.py --model_api gpt
 echo "🖥️ testing with rep model_api and nex robot"
 export O_DEATH=20
 export O_MAX_STEPS=3
+export O_NEX_MOVE_ENABLED=0
 sh s.nuke.sh
 python3 o.body.py --model_api rep --robot nex &
 python3 o.look.py --model_api rep &
