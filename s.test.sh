69a5846369cbced8bdd6401adb2782d1c1e9e1e3
hu.po <hugo.p.cmu@gmail.com>
2023-11-30 11:37:29 -0600

    test happy

--- s.test.sh
+++ s.test.sh
@@ -23,3 +23,5 @@ python3 o.body.py --model_api rep --robot test &
 python3 o.look.py --model_api rep &
 python3 o.plan.py --model_api rep &
 python3 o.talk.py --model_api rep
+# ---
+echo "🖥️✅ testing success"
