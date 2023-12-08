dda42bba0fe6e3a2d65133f02b066f35ebd4499f
hu.po <hugo.p.cmu@gmail.com>
2023-12-08 16:59:13 -0600

    sdf

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,16 +5,16 @@ model_apis=(test gpt rep)
 for model_api in "${model_apis[@]}"; do
     echo "🖥️ testing with $model_api model_api"
     bash scripts/nuke.sh
-    if [ "$model_api" = "test" ]; then
-        echo ""
-    elif [ "$model_api" = "gpt" ]; then
-        source params/gpt.sh
-    elif [ "$model_api" = "rep" ]; then
-        source params/rep.sh
-    else
-        echo "  🖥️   ❌ no tests for model_api $model_api"
-        exit 1
-    fi
+    # if [ "$model_api" = "test" ]; then
+    #     echo ""
+    # elif [ "$model_api" = "gpt" ]; then
+    #     source params/gpt.sh
+    # elif [ "$model_api" = "rep" ]; then
+    #     source params/rep.sh
+    # else
+    #     echo "  🖥️   ❌ no tests for model_api $model_api"
+    #     exit 1
+    # fi
     if ! python3 o.py --node test --model_api $model_api --robot test; then
         echo "  🖥️   ❌ testing failed with $model_api"
         exit 1
